41ebd3a34e39c1b37a7fd451e645b354fc60059b
roishmueli1 <33421651+roishmueli1@users.noreply.github.com>
2018-06-07 16:46:14 +0300

    adding createEvent function, the function incomplete

--- data/event.js
+++ data/event.js
@@ -35,4 +35,66 @@ exports.getAllEvents = (req, res) => {
     );   
 };
 exports.getEvent = (req, res) => {};
-exports.createEvent = (req, res) => {};
+
+exports.createEvent = (req, res) => {
+    var name = req.body.name;
+    var decription = req.body.decription;
+    var time = req.body.time;
+    var creator = req.body.creator;
+    var place = req.body.place;
+    var participants = req.body.participants;
+    var user_id = req.body.user_id;
+    var status = req.body.status;
+    var user_equipments = req.body.user_equipments;
+    var equipment_id = req.body.equipment_id;
+    var quantity = req.body.quantity;
+    var category = req.body.category;
+    var category1 = req.body.category1;
+    var category2 = req.body.category2;
+    var category3 = req.body.category3;
+    var required_equipment = req.body.required_equipment;
+    var equipment_id = req.body.equipment_id;
+    var max_quantity = req.body.max_quantity;
+    var min_quantity = req.body.min_quantity;
+    
+    mongoose.connect(consts.MLAB_KEY)
+    .then(
+        () => {
+            var newEvent = new Event({
+                name: name,
+                decription: decription,
+                time: Date.now(),
+                //creator: ,
+                place: place,
+                participants: [{
+                    user_id,
+                    status,
+                    user_equipments: [{
+                        equipment_id,
+                        quantity
+                    }]
+                }],
+                category: [
+                    category1, category2, category3
+                ],
+                required_equipment: [{
+                    equipment_id,
+                    max_quantity,
+                    min_quantity
+                }]
+            });
+            newEvent.save(
+                (err) => {
+                    if(err)
+                        console.log(`err: ${err}`);
+                    else {
+                        res.status(200).json(newEvent);
+                        //mongoose.disconnect();
+                    }
+                });
+        },
+        err =>{
+            console.log(`connection error: ${err}`);
+        }
+    );
+};
