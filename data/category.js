7892cd79bad3481648c99f0cc3db1a9f7641daea
roishmueli1 <33421651+roishmueli1@users.noreply.github.com>
2018-06-09 23:28:13 +0300

    added getAll and Create functions

--- data/category.js
+++ data/category.js
@@ -8,5 +8,55 @@ var mongoose = require('mongoose'),
         replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
     };
 
-exports.getAllCategories = (req, res) => {};
-exports.createCategory = (req, res) => {};
+exports.getAllCategories = (req, res) => {
+    console.log('getAllCategories');
+    // return data;
+    mongoose.connect(consts.MLAB_KEY)
+    .then(
+        () => {
+            Category.find( {},
+                (err, category) => {
+                    if (err) {
+                        console.log(`err: ${err}`);
+                        res.status(200).json(`{ err : ${err}`);
+                    }
+                    console.log(category);
+                    res.status(200).json(category);
+                   // mongoose.disconnect();
+                }
+            )
+        }, 
+        err => {
+            console.log(`connection error: ${err}`);
+            res.status(200).json(`{ connection error : ${err}`); 
+        }
+    );   
+};
+
+exports.createCategory = (req, res) => {
+    var name = req.body.name;
+    
+    mongoose.connect(consts.MLAB_KEY)
+    .then(
+        () => {
+            var newCategory = new Category({
+                name: name
+            });
+            newCategory.save(
+                (err) => {
+                    if(err)
+                        console.log(`err: ${err}`);
+                    else {
+                        res.status(200).json(newCategory);
+                        console.log(`Category ${name} added successfully`);
+                        //mongoose.disconnect();
+                    }
+                });
+        },
+        err =>{
+            console.log(`connection error: ${err}`);
+        }
+    );
+};
+
+exports.updateCategory = (req, res) => {};
