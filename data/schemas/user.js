abb269bdb88d0b346c3e03e7f2fd77f1b04a4e20
roishmueli1 <33421651+roishmueli1@users.noreply.github.com>
2018-06-12 17:12:14 +0300

    added phone and email validators

--- data/schemas/user.js
+++ data/schemas/user.js
@@ -5,8 +5,7 @@ var mongoose = require('mongoose'),
             index:1
         },
         phone: {
-            type:String,
-            unique : true
+            type:String
         },
         email: {
             type:String,
@@ -15,5 +14,20 @@ var mongoose = require('mongoose'),
         password : String,
         image : String      
     });
+
+    userSchema.path('full_name').set(
+        (val) => {
+            let sVal = String(val).toUpperCase();
+            console.log(`capitalized: ${sVal}`);
+            return sVal;
+        })
+    userSchema.path('phone').validate(
+        (val) => {
+            return /\d{3}-\d{3}-\d{4}/.test(val);
+        }, `Error validation phone number! \n The template should be xxx-xxx-xxxx`);
+    userSchema.path('email').validate(
+        (val) => {
+            return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(val);
+        }, `The e-mail field cannot be empty.`);
 
 module.exports = mongoose.model('User', userSchema);
