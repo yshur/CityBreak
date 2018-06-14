91b79bea490eea6cbef5786c65525006a1327b1a
roishmueli1 <33421651+roishmueli1@users.noreply.github.com>
2018-06-14 14:53:04 +0300

    removed all connections to database

--- data/user.js
+++ data/user.js
@@ -1,7 +1,6 @@
 'use strice';//JS engine use strict parsing
 
 var mongoose = require('mongoose'),
-    consts = require('./consts'),
     User = require('./schemas/user'),
     options = {
         server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
@@ -11,26 +10,16 @@ var mongoose = require('mongoose'),
 exports.getAllUsers = (req, res) => {
     console.log('getAllUsers');
     // return data;
-    mongoose.connect(consts.MLAB_KEY)
-    .then(
-        () => {
-            User.find( {},
-                (err, user) => {
-                    if (err) {
-                        console.log(`err: ${err}`);
-                        res.status(200).json(`{ err : ${err}`);
-                    }
-                    console.log(user);
-                    res.status(200).json(user);
-                   // mongoose.disconnect();
-                }
-            )
-        }, 
-        err => {
-            console.log(`connection error: ${err}`);
-            res.status(200).json(`{ connection error : ${err}`); 
+    User.find( {},
+        (err, user) => {
+            if (err) {
+                console.log(`err: ${err}`);
+                res.status(200).json(`{ err : ${err}`);
+            }
+            console.log(user);
+            res.status(200).json(user);
         }
-    );   
+    )
 };
 
 exports.createUser = (req, res) => {
@@ -39,64 +28,48 @@ exports.createUser = (req, res) => {
         email = req.body.email,
         password = req.body.password,
         image = req.body.image;
-    console.log('createUser');
-    console.log(`post: full_name = ${req.body.full_name},
+        console.log('createUser');
+        console.log(`post: full_name = ${req.body.full_name},
         phone = ${req.body.phone},
         email = ${req.body.email},
         password = ${req.body.password},
         image = ${req.body.image}`);
-    mongoose.connect(consts.MLAB_KEY)
-    .then(
-        () => {
-            var newUser = new User({
-                full_name: full_name,
-                phone: phone,
-                email: email,
-                password: password,
-                image: image
-            });
-            newUser.save(
-                (err) => {
-                    if(err)
-                        console.log(`err: ${err}`);
-                    else {
-                        console.log(`Saved document: ${newUser}`);
-                        res.status(200).json(newUser);
-                        //mongoose.disconnect();
-                    }
-                });
-        },
-        err =>{
-            console.log(`connection error: ${err}`);
-        }
-    );
+    
+    var newUser = new User({
+        full_name: full_name,
+        phone: phone,
+        email: email,
+        password: password,
+        image: image
+    });
+    newUser.save(
+        (err) => {
+            if(err)
+                console.log(`err: ${err}`);
+            else {
+                console.log(`Saved document: ${newUser}`);
+                res.status(200).json(newUser);
+            }
+        });
 };
 
 exports.getUser = (req, res) => {
     var userid = req.params.userid;
     console.log('getUser');
     console.log(`get: userid = ${req.params.userid}`);
-    mongoose.connect(consts.MLAB_KEY)
-    .then(
-        () => {
-            User.findById(userid,
-                (err, user) => {
-                    if (err) {
-                        console.log(`err: ${err}`);
-                        res.status(200).json(`{ err : ${err}`);
-                    }
-                    console.log(user);
-                    res.status(200).json(user);
-                   // mongoose.disconnect();
-                }
-            )
-        }, 
-        err => {
-            console.log(`connection error: ${err}`);
-            res.status(200).json(`{ connection error : ${err}`); 
+
+    User.findById(userid,
+        (err, user) => {
+            if (err) {
+                console.log(`err: ${err}`);
+                res.status(200).json(`{ err : ${err}`);
+            }
+            console.log(user);
+            res.status(200).json(user);
         }
-    ); 
+    )
 };
+
 exports.updateUser = (req, res) => {
     var userid = req.body.userid;
     var full_name = req.body.full_name;
@@ -104,42 +77,35 @@ exports.updateUser = (req, res) => {
         email = req.body.email,
         password = req.body.password,
         image = req.body.image;
-    mongoose.connect(consts.MLAB_KEY)
-    .then(
-        () => {
-                var conditions = {phone: phone, email:email, password:password, image:image}
-                opts = {multi: true};
-                User.findByIdAndUpdate(userid, conditions, opts,
-                    (err, user) => {
-                        if(err)
-                            console.log(`err: ${err}`);
-                        else {
-                            console.log(`Updated user: ${user}`)
-                            res.status(200).json(user);
-                        }
-                    })
-        });
 
-}
+    var conditions = {phone: phone, email:email, password:password, image:image}
+    opts = {multi: true};
+    User.findByIdAndUpdate(userid, conditions, opts,
+        (err, user) => {
+            if(err)
+                console.log(`err: ${err}`);
+            else {
+                console.log(`Updated user: ${user}`)
+                res.status(200).json(user);
+            }
+        })
+};
+
 exports.deleteUserByName = (req, res) => {
     var full_name = req.body.full_name;
-    mongoose.connect(consts.MLAB_KEY)
-    .then(
-        () => {
-            var conditions = {full_name: full_name};
+    var conditions = {full_name: full_name};
 
-            User.remove(conditions,
-                (err) => {
-                    if(err)
-                        console.log(`err: ${err}`);
-                    else {
-                        console.log(`Removed document`);
-                        User.findOne({full_name: full_name},
-                            (err) => {
-                                console.log(`Removed user ${full_name} `);
-                                res.status(200).json(`Removed user ${full_name}`);
-                            });
-                    };
-                });
+    User.remove(conditions,
+        (err) => {
+            if(err)
+                console.log(`err: ${err}`);
+            else {
+                console.log(`Removed document`);
+                User.findOne({full_name: full_name},
+                    (err) => {
+                        console.log(`Removed user ${full_name} `);
+                        res.status(200).json(`Removed user ${full_name}`);
+                    });
+            };
         });
 };
