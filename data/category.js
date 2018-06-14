dba4cb93a28947613b1bd25056aa341533e69534
roishmueli1 <33421651+roishmueli1@users.noreply.github.com>
2018-06-14 14:52:29 +0300

    removed all connections to database

--- data/category.js
+++ data/category.js
@@ -1,7 +1,6 @@
 'use strice';//JS engine use strict parsing
 
 var mongoose = require('mongoose'),
-    consts = require('./consts'),
     Category = require('./schemas/category'),
     options = {
         server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
@@ -10,101 +9,65 @@ var mongoose = require('mongoose'),
 
 exports.getAllCategories = (req, res) => {
     console.log('getAllCategories');
-    mongoose.connect(consts.MLAB_KEY)
-    .then(
-        () => {
-            Category.find( {},
-                (err, category) => {
-                    if (err) {
-                        console.log(`err: ${err}`);
-                        res.status(200).json(`{ err : ${err}`);
-                    }
-                    console.log(category);
-                    res.status(200).json(category);
-                   // mongoose.disconnect();
-                }
-            )
-        }, 
-        err => {
-            console.log(`connection error: ${err}`);
-            res.status(200).json(`{ connection error : ${err}`); 
+    Category.find( {},
+        (err, category) => {
+            if (err) {
+                console.log(`err: ${err}`);
+                res.status(200).json(`{ err : ${err}`);
+            }
+            console.log(category);
+            res.status(200).json(category);
         }
-    ); 
+    )
 };
 exports.createCategory = (req, res) => {
     var name = req.body.name;
     console.log('createCategory');
     console.log(`post: name = ${req.body.name}`);
-    mongoose.connect(consts.MLAB_KEY)
-    .then(
-        () => {
-            var newCategory = new Category({
-                name: name
-            });
-            newCategory.save(
-                (err) => {
-                    if(err)
-                        console.log(`err: ${err}`);
-                    else {
-                        console.log(`Saved document: ${newCategory}`);
-                        res.status(200).json(newCategory);
-                        //mongoose.disconnect();
-                    }
-                });
-        },
-        err =>{
-            console.log(`connection error: ${err}`);
-        }
-
-    );    
+    var newCategory = new Category({
+        name: name
+    });
+    newCategory.save(
+        (err) => {
+            if(err)
+                console.log(`err: ${err}`);
+            else {
+                console.log(`Saved document: ${newCategory}`);
+                res.status(200).json(newCategory);
+            }
+        });    
 };
 
 exports.getEquipmentsByCategory = (req, res) => {
     var category = req.params.category;
     console.log('getEquipmentsByCategory');
     console.log(`get: category = ${req.params.category}`);
-    mongoose.connect(consts.MLAB_KEY)
-    .then(
-        () => {
-            Categroy.find( { name: { $eq: category } },
-                (err, equipment) => {
-                    if (err) {
-                        console.log(`err: ${err}`);
-                        res.status(200).json(`{ err : ${err}`);
-                    }
-                    console.log(equipment);
-                    res.status(200).json(equipment);
-                   // mongoose.disconnect();
-                }
-            )
-        }, 
-        err => {
-            console.log(`connection error: ${err}`);
-            res.status(200).json(`{ connection error : ${err}`); 
+    
+    Category.find( { name: { $eq: category } },
+        (err, equipment) => {
+            if (err) {
+                console.log(`err: ${err}`);
+                res.status(200).json(`{ err : ${err}`);
+            }
+            console.log(equipment);
+            res.status(200).json(equipment);
         }
-    ); 
+    )
 };
-
 
 exports.addEquipmentToCategory = (req, res) => {
     var equipment = req.body.equipment;
     var categoryid = req.body.categoryid;
-    mongoose.connect(consts.MLAB_KEY)
-    .then(
-        () => {
-                var conditions = { $push: { equipments: equipment } },
-                opts = {multi: true};
-                Category.findByIdAndUpdate(categoryid, conditions, opts,
-                    (err, category) => {
-                        if(err)
-                            console.log(`err: ${err}`);
-                        else {
-                            console.log(`Updated user: ${category}`)
-                            res.status(200).json(category);
-
-
-                        }
-                    })
-        });
-
+    
+    var conditions = { $push: { equipments: equipment } },
+    opts = {multi: true};
+    Category.findByIdAndUpdate(categoryid, conditions, opts,
+        (err, category) => {
+            if(err)
+                console.log(`err: ${err}`);
+            else {
+                console.log(`Updated user: ${category}`)
+                res.status(200).json(category);
+            }
+        })
 }
