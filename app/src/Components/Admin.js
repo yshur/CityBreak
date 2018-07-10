e6281877d661fc1d71cdc262fa20d417fca0b73d
roishmueli1 <33421651+roishmueli1@users.noreply.github.com>
2018-07-10 12:24:27 +0300

    admin.js incomplete

--- app/src/Components/Admin.js
+++ app/src/Components/Admin.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react'
 import MdSave from 'react-icons/lib/md/save'
+import MdAdd from 'react-icons/lib/md/add'
+import Background from './bk.jpg';
 
 
 
+var bk = {
+    width:"100%",
+    height:"100%px",
+    backgroundImage: `url(${Background})`
+}
 class Admin extends Component{
     render() {
         return (
-            <div className="admin">
+            <div className="admin" style = {bk}>
                 <h1> Admin Interface </h1>
+                    <h5> Manage your events </h5>
+                    <button className= "add"><MdAdd/></button>
                     <div className="adminOp1">
                         <button><MdSave/></button><p>Create event</p>
                     </div>
