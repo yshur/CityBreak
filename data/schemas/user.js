c94c2999781daebf31b16a80765ca9d541511032
roishmueli1 <33421651+roishmueli1@users.noreply.github.com>
2019-03-20 23:55:40 +0200

    all user routes almost done

---
+++ data/schemas/user.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose'),
+    userSchema = new mongoose.Schema({
+        full_name: {
+            type:String,
+            index:1
+        },
+        phone: {
+            type:String,
+            unique : true
+        },
+        email: {
+            type:String,
+            unique : true
+        },
+        password : String,
+        image : String
+    });
+
+    userSchema.path('phone').validate(
+        (val) => {
+            return /\d{3}-\d{3}-\d{4}/.test(val);
+        }, `Error validation phone number! \n The template should be xxx-xxx-xxxx`);
+    userSchema.path('email').validate(
+        (val) => {
+            return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(val);
+        }, `The e-mail field cannot be empty.`);
+
+module.exports = mongoose.model('User', userSchema);
---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require ('express'),
+	bodyParser = require('body-parser'),
+	app = express(),
+	port = process.env.PORT || 3000,
+	user = require('./data/user');
+    require('./database');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/includes', express.static(`${__dirname}/public`));
+app.use('/', express.static('./'));
+
+app.use( (req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers",
+	"Origin, X-Requested-With, Content-Type, Accept");
+	res.set("Content-Type", "application/json");
+	next();
+});
+
+app.all('*', (req, res, next) => {
+  console.log("runs for all HTTP verbs first");
+  next();
+});
+
+app.get('/', (req,res) => {
+	console.log(`__dirname: ${__dirname}`);
+	res.status(200).sendFile(`${__dirname}/index.html`);
+});
+
+/*---------------- User Routes ----------------*/
+app.get('/getAllUsers', user.getAllUsers);
+app.delete('/deleteUser/', user.deleteUser);
+app.post('/createUser/', user.createUser);
+app.get('/getUser/:userid', user.getUser);
+app.post('/updateUser/', user.updateUser);
+
+/*--------------- Others Routes -------------*/
+app.all('*', function(req, res) {
+  var error = {
+    "error":"url not found",
+  	"help": "TBD"
+  };
+  res.status(200).json(error);
+});
+
+app.listen(port, () => {
+	console.log(`listening on port ${port}`);
+});
