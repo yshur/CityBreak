d8337e942b46d3f4e196354a051b43a84977baa3
yshor <yshur@golantelecom.co.il>
2019-04-01 13:21:02 +0300

    adding tours components

--- nodeApp/data/index.js
+++ nodeApp/data/index.js
@@ -1,12 +1,8 @@
 'use strice';//JS engine use strict parsing
-// var data = require('./data.json');
 var mongoose = require('mongoose'),
     consts = require('./consts'),
     User = require('./schemas/user'),
-    Chat = require('./schemas/chat'),
-    Event = require('./schemas/event'),
-    Category = require('./schemas/category'),
-    Equipment = require('./schemas/equipment'),
+    Tour = require('./schemas/tour'),
     options = {
         server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
         replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
