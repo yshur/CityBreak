e3d2af724b76d9b6617229b408a6af15a3f24428
yshor <yshur@golantelecom.co.il>
2019-06-17 01:02:50 +0300

    update crud of point & tour

--- nodeApp/data/point.js
+++ nodeApp/data/point.js
@@ -1,7 +1,7 @@
 'use strice';//JS engine use strict parsing
 
 var mongoose = require('mongoose'),
-	url = require('url'),
+		url = require('url'),
     User = require('./schemas/user'),
     Point = require('./schemas/point'),
     Tour = require('./schemas/tour');
@@ -21,7 +21,8 @@ exports.createPoint = (req, res) => {
         area: 			req.body.area,
         sub_area: 		req.body.sub_area,
         accessibility: 	req.body.accessibility,
-        recommended_season: req.body.recommended_season
+        recommended_season: req.body.recommended_season,
+				loc: req.body.loc
     });
     console.log('createPoint:');
     console.log(newPoint);
@@ -38,13 +39,13 @@ exports.createPoint = (req, res) => {
         });
 };
 exports.getPoints = (req, res) => {
-    console.log('getPoints');
+  console.log('getPoints');
 	var queryData = url.parse(req.url, true).query;
 	var params = {};
 	var show = {
 		"_id":1, "name":1,"about":1,"image_url":1,"tags":1,
 		"address":1,"area":1,"sub_area":1,"accessibility":1,
-		"recommended_season":1 
+		"recommended_season":1
 		};
 	if (queryData.area) {
 		params.area = queryData.area;
@@ -90,7 +91,7 @@ exports.getPoints = (req, res) => {
 exports.getPoint = (req, res) => {
 	console.log(`getPoint: pointid = ${req.params.pointid}`);
     var pointid = req.params.pointid;
-    
+
     Point.findById(pointid,
         (err, point) => {
             if (err) {
@@ -139,7 +140,7 @@ exports.updatePoint = (req, res) => {
 	if (req.body.area) {
 		params.area = req.body.area;
 	}
-	if (req.body.accessibility) {
+	if (req.body.accessibility != null) {
 		params.accessibility = req.body.accessibility;
 	}
 	if (req.body.sub_area) {
@@ -151,7 +152,7 @@ exports.updatePoint = (req, res) => {
 	if (req.body.loc) {
 		params.loc = req.body.loc;
 	}
-	
+	console.log(params);
     var opts = {
         new: true
     };
@@ -169,7 +170,7 @@ exports.updatePoint = (req, res) => {
 exports.deletePoint = (req, res) => {
 	console.log(`deletePoint: pointid = ${req.params.pointid}`);
     var pointid = req.params.pointid;
-	
+
     Point.findByIdAndRemove(pointid, (err, point) => {
           // As always, handle any potential errors:
           if (err) return res.status(300).json(err);
--- nodeApp/data/tour.js
+++ nodeApp/data/tour.js
@@ -1,17 +1,22 @@
 'use strice';//JS engine use strict parsing
 
 var mongoose = require('mongoose'),
+    url = require('url'),
     User = require('./schemas/user'),
     Tour = require('./schemas/tour'),
     Point = require('./schemas/point');
 
-
 exports.createTour = (req, res) => {
     var newTour = new Tour({
         name:       req.body.name,
+        about:      req.body.about,
+        description: req.body.description,
         creator:    req.body.creator,
         area: 			req.body.area,
-        start_point: req.body.start_point
+        sub_area:   req.body.sub_area,
+        start_point: req.body.start_point,
+        accessibility: req.body.accessibility,
+        recommended_season: req.body.recommended_season
     });
     console.log('Create Tour');
     newTour.save(
@@ -23,6 +28,115 @@ exports.createTour = (req, res) => {
                 console.log(`Saved document:`);
                 res.status(200).json(newTour);
                 console.log(newTour);
+            }
+        });
+};
+exports.getTours = (req, res) => {
+    console.log('getTours');
+    var queryData = url.parse(req.url, true).query;
+    var params = {};
+    var show = {
+      "_id":1, "name":1,"about":1,"tags":1,"duration":1,
+      "distance":1,"area":1,"sub_area":1,"accessibility":1,
+      "recommended_season":1
+      };
+
+    if (queryData.area) {
+      params.area = queryData.area;
+    }
+    if (queryData.sub_area) {
+      params.sub_area = queryData.sub_area;
+    }
+    if (queryData.accessibility) {
+      params.accessibility = queryData.accessibility;
+    }
+    if (queryData.tags) {
+      params.tags = { $in: queryData.tags.split(",") };
+    }
+    // if (queryData.near) {
+    //   var near = queryData.near.split(",").map(function(v) {
+    //     return Number(v);
+    //   });
+    //   params.loc = {
+    //      $near: {
+    //     $maxDistance: 1000,
+    //     $geometry: {
+    //      type: "Point",
+    //      coordinates: near
+    //     }
+    //      }
+    //    };
+    //   // params.loc = { $near: { $geometry: {type: 'Point', coordinates:queryData.near.split(",") }, $maxDistance: 10 } };
+    //   // params.loc = { $near: {type: 'Point', coordinates: queryData.near.split(",") } };
+    //   console.log(params.loc.$near.$geometry);
+    // }
+
+    console.log(params);
+    var q = Tour.find(params, show);
+    q.exec(function(err, tours)  {
+      if (err) {
+        console.log(`err: ${err}`);
+        res.status(200).json(`{ err : ${err}`);
+      }
+      console.log(tours);
+      res.status(200).json(tours);
+    });
+};
+exports.getTour = (req, res) => {
+    var tourid = req.params.tourid;
+    console.log(`getTour: tourid = ${req.params.tourid}`);
+    Tour.findById(tourid,
+        (err, tour) => {
+            if (err) {
+                console.log(`err: ${err}`);
+                res.status(200).json(`{ err : ${err} }`);
+            }
+            console.log(tour);
+            res.status(200).json(tour);
+        }
+    )
+};
+exports.updateTour = (req, res) => {
+	var tourid = req.params.tourid;
+	console.log(`updateTour: tourid = ${req.params.tourid}`);
+    var params = {};
+	if (req.body.name) {
+		params.name = req.body.name;
+	}
+	if (req.body.about) {
+		params.about = req.body.about;
+	}
+	if (req.body.description) {
+		params.description = req.body.description;
+	}
+	if (req.body.area) {
+		params.area = req.body.area;
+	}
+	if (req.body.accessibility != null) {
+		params.accessibility = req.body.accessibility;
+	}
+	if (req.body.sub_area) {
+		params.sub_area = req.body.sub_area;
+	}
+	if (req.body.recommended_season) {
+		params.recommended_season = req.body.recommended_season;
+	}
+	if (req.body.start_point) {
+		params.start_point = req.body.start_point;
+	}
+
+    var opts = {
+        new: true
+    };
+    console.log(params);
+    Tour.findByIdAndUpdate(tourid, params, opts,
+        (err, tour) => {
+            if(err) {
+                console.log(`err: ${err}`);
+                res.status(300).json(err);
+            } else {
+                console.log(`Updated tour: ${tour}`)
+                res.status(200).json(tour);
             }
         });
 };
@@ -42,34 +156,7 @@ exports.deleteTour = (req, res) => {
           }
       });
 };
-exports.getTours = (req, res) => {
-    console.log('getTours');
-    // return data;
-    Tour.find( {},
-        (err, tour) => {
-            if (err) {
-                console.log(`err: ${err}`);
-                res.status(200).json(`{ err : ${err} }`);
-            }
-            console.log(tour);
-            res.status(200).json(tour);
-        }
-    )
-};
-exports.getTour = (req, res) => {
-    var tourid = req.params.tourid;
-    console.log(`getTour: tourid = ${req.params.tourid}`);
-    Tour.findById(tourid,
-        (err, tour) => {
-            if (err) {
-                console.log(`err: ${err}`);
-                res.status(200).json(`{ err : ${err} }`);
-            }
-            console.log(tour);
-            res.status(200).json(tour);
-        }
-    )
-};
+
 exports.addPoint = (req, res) => {
   var tourid = req.params.tourid,
       pointid = req.params.pointid;
@@ -92,87 +179,6 @@ exports.addPoint = (req, res) => {
           });
 }
 
-
-exports.getRandomTours = (req, res) => {
-    console.log('getRandomTours');
-	var q = Tour.find().limit(2);
-	q.exec(function(err, tours)  {
-		if (err) {
-			console.log(`err: ${err}`);
-			res.status(200).json(`{ err : ${err}`);
-		}
-		console.log(tours);
-		res.status(200).json(tours);
-	});
-};
-exports.getLongTours = (req, res) => {
-    console.log('getLongTours');
-	var q = Tour.find({"isRoute": true,"source": { $not: { $eq: "Jeepolog" } } }).limit(2);
-	q.exec(function(err, tours)  {
-		if (err) {
-			console.log(`err: ${err}`);
-			res.status(200).json(`{ err : ${err}`);
-		}
-		console.log(tours);
-		res.status(200).json(tours);
-	});
-};
-exports.getAllTours = (req, res) => {
-    console.log('getAllTours');
-	var q = Tour.find({"isRoute": true,"source": { $not: { $eq: "Jeepolog" } } },
-		{"id":1,"source":1,"lengthInKm":1,"description":1,"imagesUrls":1,"title":1,"category":1,"location":1 }
-		).limit(10);
-	q.exec(function(err, tours)  {
-		if (err) {
-			console.log(`err: ${err}`);
-			res.status(200).json(`{ err : ${err}`);
-		}
-		console.log(tours);
-		res.status(200).json(tours);
-	});
-};
-exports.getNorthTours = (req, res) => {
-    console.log('getNorthTours');
-	var q = Tour.find({"isRoute": true,"source": { $not: { $eq: "Jeepolog" } },"location.lat": { $gt: 32.540422 } },
-		{"id":1,"source":1,"lengthInKm":1,"description":1,"imagesUrls":1,"title":1,"category":1,"location":1 }
-		).limit(10);
-	q.exec(function(err, tours)  {
-		if (err) {
-			console.log(`err: ${err}`);
-			res.status(200).json(`{ err : ${err}`);
-		}
-		console.log(tours);
-		res.status(200).json(tours);
-	});
-};
-exports.getSouthTours = (req, res) => {
-    console.log('getSouthTours');
-	var q = Tour.find({"isRoute": true,"source": { $not: { $eq: "Jeepolog" } },"location.lat": { $lt: 31.864304 } },
-		{"id":1,"source":1,"lengthInKm":1,"description":1,"imagesUrls":1,"title":1,"category":1,"location":1 }
-		).limit(10);
-	q.exec(function(err, tours)  {
-		if (err) {
-			console.log(`err: ${err}`);
-			res.status(200).json(`{ err : ${err}`);
-		}
-		console.log(tours);
-		res.status(200).json(tours);
-	});
-};
-exports.getCenterTours = (req, res) => {
-    console.log('getCenterTours');
-	var q = Tour.find({"isRoute": true,"source": { $not: { $eq: "Jeepolog" } },"location.lat": { $lt: 32.540422, $gt: 31.864304 } },
-		{"id":1,"source":1,"lengthInKm":1,"description":1,"imagesUrls":1,"title":1,"category":1,"location":1 }
-		).limit(10);
-	q.exec(function(err, tours)  {
-		if (err) {
-			console.log(`err: ${err}`);
-			res.status(200).json(`{ err : ${err}`);
-		}
-		console.log(tours);
-		res.status(200).json(tours);
-	});
-};
 exports.setTimeTour = (req, res) => {
     var tourid = req.body.tourid,
         time = req.body.time;
@@ -262,7 +268,6 @@ exports.addEqTour = (req, res) => {
             }
         });
 };
-
 
 exports.getChat = (req, res) => {
     var tourid = req.params.tourid;
@@ -363,25 +368,6 @@ exports.setUserEquip = (req, res) => {
             }
         });
 };
-// exports.deleteTour = (req, res) => {
-//     var tourid = req.body.tourid;
-//     var conditions = {_id: tourid};
-//
-//     Tour.remove(conditions,
-//         (err) => {
-//             if(err){
-//                 console.log(`err: ${err}`);
-//                 res.status(300).json(err);
-//             } else {
-//                 console.log(`Removed document`);
-//                 User.findOne({_id: tourid},
-//                     (err) => {
-//                         console.log(`Removed tour id=${tourid} `);
-//                         res.status(200).json({result:`Removed ${tourid}`});
-//                     });
-//             };
-//         });
-// };
 exports.getUserTour = (req, res) => {
     var userid = req.body.userid;
     console.log('getUserTour');
