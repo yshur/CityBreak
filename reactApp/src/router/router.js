54a43c45b6f160b006b2d88162af48c54f836c26
yshor <yshur@golantelecom.co.il>
2019-07-03 20:17:23 +0300

    delete unnecessary components

--- reactApp/src/router/router.js
+++ reactApp/src/router/router.js
@@ -3,12 +3,9 @@ import Cookies from 'js-cookie'
 import { Route } from "react-router-dom";
 import UsersList from "../Components/UsersList";
 import SignUp from "../Components/SignUp";
-import SignUpAdmin from "../Components/SignUpAdmin";
 import Home from "../Components/Home";
 import Header from "../Components/Header";
 import Login from "../Components/Login";
-import SignInAdmin from "../Components/SignInAdmin";
-import AdminHome from "../Components/AdminHome";
 import Main from "../Components/Main";
 import FilterableTourTable from "../Components/FilterableTourTable";
 import FilterablePointTable from "../Components/FilterablePointTable";
@@ -24,15 +21,11 @@ const ReactRouter =()=>{
         <Header />
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
-            <Route exact path="/Header" component={Header}/>
+            <Route exact path="/header" component={Header}/>
             <Route exact path="/users" component={UsersList}/>
             <Route exact path="/signup" component={SignUp}/>
-            <Route exact path="/signupadmin" component={SignUpAdmin}/>
             <Route exact path="/home" component={Home}/>
-            <Route exact path="/header" component={Header}/>
             <Route exact path="/TourPage" component={TourPage}/>
-            <Route exact path="/signinadmin" component={SignInAdmin}/>
-            <Route exact path="/adminhome" component={AdminHome}/>
             <Route exact path="/main" component={Main}/>
             <Route exact path="/alltours" component={FilterableTourTable}/>
             <Route exact path="/allpoints" component={FilterablePointTable}/>
